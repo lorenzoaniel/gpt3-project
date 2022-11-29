676234626d6649e89b1ed7f9aa7094f939a46149
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-28 20:22:16 -0700

    Create Links.tsx
    changed NSlinks to jsut Links and applying variants to style it using props

---
+++ src/Components/Links/Links.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+type LinksProps = {
+	linkNameProp?: string;
+	variant: string;
+};
+
+type LinksStyledProps = {
+	variant: string;
+};
+
+interface LinksVariantsType {
+	LinksContainer: any;
+}
+
+const Links = (props: LinksProps) => {
+	const { linkNameProp, variant } = props;
+
+	return <LinksContainer variant={variant}>{linkNameProp}</LinksContainer>;
+};
+
+const LinksVariants: LinksVariantsType = {
+	LinksContainer: {
+		navStyle: {
+			fontSize: "18px",
+		},
+	},
+};
+
+const LinksContainer = styled.a<LinksStyledProps>`
+	font-size: ${(props) =>
+		LinksVariants.LinksContainer[props.variant as keyof LinksVariantsType].fontSize};
+`;
+
+export default Links;
