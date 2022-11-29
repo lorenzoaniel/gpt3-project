1258e0e8904701b0b70fe7c81c9c217bcc3613cb
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-28 20:07:01 -0700

    Create GlobalStyle.tsx
    converted to tsx

---
+++ src/Styles/GlobalStyle/GlobalStyle.tsx
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    /*FONTS*/
+
+    --default-font: 'Manrope', sans-serif; 
+
+    /*STYLES*/
+    font-family: var(--default-font);
+
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    scrollbar-width: none;
+  }
+
+  html {
+    height: 100vh;
+    width: 100vw;
+
+    body{
+      background-color: red;
+
+      #root{
+        background-color: green;
+        height: 5571px;
+        width: 100%;
+      }
+    }
+  }
+`;
