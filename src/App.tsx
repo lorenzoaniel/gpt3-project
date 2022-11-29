97dc5882e9b6f42da3c5aa7ae953b40332e2cd30
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-28 20:04:54 -0700

    App: added Navbar section
    added Navbar section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+import Navbar from "./Sections/Navbar/Navbar";
+
 import { GlobalStyle } from "./Styles/GlobalStyle/GlobalStyle";
+
+const AppContainer = styled.div`
+	background-color: yellow;
+	height: inherit;
+	width: inherit;
+
+	display: flex;
+	flex-direction: column;
+	padding: 0 10%;
+`;
 
 function App() {
 	return (
 		<>
 			<GlobalStyle />
-			<div className="App">test</div>
+			<AppContainer>
+				<Navbar />
+			</AppContainer>
 		</>
 	);
 }
