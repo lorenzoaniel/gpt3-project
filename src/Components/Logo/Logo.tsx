eeb09560e217020dc493b34e85cc104c71acf188
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-28 20:05:28 -0700

    Create Logo.tsx
    base logo comp

---
+++ src/Components/Logo/Logo.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+
+type LogoProps = {
+	logoNameProp?: string;
+	variant: string;
+};
+
+interface LogoVariantsType {
+	LogoLetters: any;
+	LogoContainer: any;
+	LogoMiniContainer: any;
+}
+
+const Logo = (props: LogoProps) => {
+	const { variant, logoNameProp } = props;
+
+	return (
+		<LogoContainer>
+			<LogoMiniContainer>
+				<LogoLetters variant={variant}>{logoNameProp}</LogoLetters>
+			</LogoMiniContainer>
+		</LogoContainer>
+	);
+};
+
+const LogoVariants: LogoVariantsType = {
+	LogoContainer: {},
+	LogoMiniContainer: {},
+	LogoLetters: {
+		navStyle: {
+			fontSize: "18px",
+		},
+		footerStyle: {
+			fontSize: "24px",
+		},
+	},
+};
+
+export const LogoContainer = styled.div`
+	background-color: green;
+	height: 58px;
+	width: 65px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const LogoMiniContainer = styled.span`
+	background-color: orange;
+	color: #ffffff;
+`;
+
+export const LogoLetters = styled.a<LogoProps>`
+	font-size: ${(props) =>
+		LogoVariants.LogoLetters[props.variant as keyof LogoVariantsType].fontSize};
+	font-weight: 900;
+`;
+
+export default Logo;
