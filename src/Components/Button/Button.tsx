978518429936c0c3d3a5f05a2feda5a34226b153
lorenzoaniel <lorenzoaniel@gmail.com>
2022-11-29 21:57:49 -0700

    button: basic code
    added basic code

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import styled from "styled-components";
 
-const Button = () => {
-	return <div>Button</div>;
+type ButtonProps = {
+	buttonNameProp: string;
+	variant: string;
 };
+
+type ButtonStyledProps = {
+	variant: string;
+};
+
+interface ButtonVariantsType {
+	ButtonContainer: any;
+}
+
+const Button = (props: ButtonProps) => {
+	const { buttonNameProp, variant } = props;
+
+	return <ButtonContainer variant={variant}>{buttonNameProp}</ButtonContainer>;
+};
+
+const ButtonVariants: ButtonVariantsType = {
+	ButtonContainer: {
+		navStyle: {
+			height: "58px",
+			width: "152px",
+			borderRadius: "5px",
+			backgroundColor: "#FF4820",
+		},
+		navStyleTransparent: {
+			height: "58px",
+			width: "152px",
+		},
+	},
+};
+
+const ButtonContainer = styled.button<ButtonStyledProps>`
+	background-color: ${(props) =>
+		ButtonVariants.ButtonContainer[props.variant as keyof ButtonVariantsType].backgroundColor};
+	height: ${(props) =>
+		ButtonVariants.ButtonContainer[props.variant as keyof ButtonVariantsType].height};
+	width: ${(props) =>
+		ButtonVariants.ButtonContainer[props.variant as keyof ButtonVariantsType].width};
+`;
 
 export default Button;
