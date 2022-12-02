7bb37be9a9a0ebdc131b6704820aa17e287f8997
lorenzoaniel <lorenzoaniel@gmail.com>
2022-12-01 22:23:55 -0700

    Update Button.tsx

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -27,21 +27,32 @@ const ButtonVariants: ButtonVariantsType = {
 			width: "152px",
 			borderRadius: "5px",
 			backgroundColor: "#FF4820",
+			fontSize: "18px",
 		},
 		navStyleTransparent: {
 			height: "58px",
 			width: "152px",
+			borderRadius: "5px",
+			backgroundColor: "transparent",
+			fontSize: "18px",
 		},
 	},
 };
 
 const ButtonContainer = styled.button<ButtonStyledProps>`
+	border: none;
+	color: #ffffff;
+
 	background-color: ${(props) =>
 		ButtonVariants.ButtonContainer[props.variant as keyof ButtonVariantsType].backgroundColor};
 	height: ${(props) =>
 		ButtonVariants.ButtonContainer[props.variant as keyof ButtonVariantsType].height};
 	width: ${(props) =>
 		ButtonVariants.ButtonContainer[props.variant as keyof ButtonVariantsType].width};
+	font-size: ${(props) =>
+		ButtonVariants.ButtonContainer[props.variant as keyof ButtonVariantsType].fontSize};
+	border-radius: ${(props) =>
+		ButtonVariants.ButtonContainer[props.variant as keyof ButtonVariantsType].borderRadius};
 `;
 
 export default Button;
