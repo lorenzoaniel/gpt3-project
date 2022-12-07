715b781bafbfa4fd6b58b911d682a416498c8790
lorenzoaniel <lorenzoaniel@gmail.com>
2022-12-06 21:17:34 -0700

    Create Line.tsx
    rainbow line above headings usually

---
+++ src/Components/Line/Line.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+type LineProps = {
+	variant?: string;
+};
+
+type LineStyledProps = {
+	variant: string;
+};
+
+interface LineVariantsType {
+	MainContainer: any;
+}
+
+const Line = (props: LineProps) => {
+	const { variant = "default" } = props;
+
+	return <MainContainer variant={variant} />;
+};
+
+const LineVariants: LineVariantsType = {
+	/* variants of the component can be applied on the 'variant' prop as a string. ex variant={"navStyle"} */
+	MainContainer: {
+		default: "" /* default to rely on base style stated on style component */,
+		featureOne: `
+      grid-area: line;
+      align-self: flex-end;
+    `,
+	},
+};
+
+const MainContainer = styled.div<LineStyledProps>`
+	background: linear-gradient(103.22deg, #ae67fa -13.86%, #f49867 99.55%), #ffffff;
+	width: 38px;
+	height: 3px;
+
+	${(props) => LineVariants.MainContainer[props.variant as keyof LineVariantsType]};
+`;
+
+export default Line;
