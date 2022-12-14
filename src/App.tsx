c5f352d2073fc53adb316e0a081219f56f1f3248
lorenzoaniel <lorenzoaniel@gmail.com>
2022-12-14 16:06:26 -0700

    APP: finished initial look
    will now work on mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,24 @@ import styled from "styled-components";
 
 import Navbar from "./Sections/Navbar/Navbar";
 import Header from "./Sections/Header/Header";
-
-import { GlobalStyle } from "./Styles/GlobalStyle/GlobalStyle";
 import CompanyBrands from "./Sections/CompanyBrands/CompanyBrands";
 import FeatureOne from "./Sections/FeatureOne/FeatureOne";
+import FeatureTwo from "./Sections/FeatureTwo/FeatureTwo";
+import FeatureThree from "./Sections/FeatureThree/FeatureThree";
+import CTA from "./Sections/CTA/CTA";
+import Blog from "./Sections/Blog/Blog";
+import Footer from "./Sections/Footer/Footer";
+
+import { GlobalStyle } from "./Styles/GlobalStyle/GlobalStyle";
 
 const AppContainer = styled.main`
 	background-color: var(--main-background-color);
-	height: inherit;
+	height: fit-content;
 	width: inherit;
 
 	display: flex;
 	flex-direction: column;
-	padding: 0 10%;
+	padding: 0 5%;
 `;
 
 function App() {
@@ -27,7 +32,12 @@ function App() {
 				<Header />
 				<CompanyBrands />
 				<FeatureOne />
+				<FeatureTwo />
+				<FeatureThree />
+				<CTA />
+				<Blog />
 			</AppContainer>
+			<Footer />
 		</>
 	);
 }
