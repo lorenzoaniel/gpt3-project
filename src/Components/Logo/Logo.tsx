305dcc4296b5cd9c7711e5793a4a835c3a2b0785
lorenzoaniel <lorenzoaniel@gmail.com>
2022-12-14 16:14:33 -0700

    Update Logo.tsx

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -13,54 +13,68 @@ type LogoStyledProps = {
 interface LogoVariantsType {
 	LogoLetters: any;
 	LogoContainer: any;
-	LogoMiniContainer: any;
 }
 
 const Logo = (props: LogoProps) => {
 	const { logoNameProp, variant = "default" } = props;
 
 	return (
-		<LogoContainer>
-			<LogoMiniContainer>
-				<LogoLetters variant={variant}>{logoNameProp}</LogoLetters>
-			</LogoMiniContainer>
+		<LogoContainer variant={variant}>
+			<LogoLetters variant={variant}>{logoNameProp}</LogoLetters>
 		</LogoContainer>
 	);
 };
 
-const LogoVariants: LogoVariantsType = {
-	/* variants of the component can be applied on the 'variant' prop as a string. ex variant={"navStyle"} */
-	LogoContainer: {},
-	LogoMiniContainer: {},
-	LogoLetters: {
-		default: `` /* default to rely on base style stated on style component */,
-		navStyle: `
-			font-size: 22px,
+const LogoMixinVariants: LogoVariantsType = {
+	LogoContainer: {
+		default: `
+			height: 100%;
+			width: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
 		`,
-		footerStyle: `
-			font-size: 24px,
+	},
+	LogoLetters: {
+		default: `
+			font-weight: 900;
+			color: #ffffff;
 		`,
 	},
 };
 
-export const LogoContainer = styled.div`
+const LogoVariants: LogoVariantsType = {
+	/* variants of the component can be applied on the 'variant' prop as a string. ex variant={"navStyle"} */
+	LogoContainer: {
+		default: `` /* default to rely on base style stated on style component */,
+		navStyle: `
+			${LogoMixinVariants.LogoContainer.default}
+		`,
+		footerStyle: `
+			${LogoMixinVariants.LogoContainer.default}
+			height: fit-content;
+			width: fit-content;
+		`,
+	},
+	LogoLetters: {
+		default: `` /* default to rely on base style stated on style component */,
+		navStyle: `
+			${LogoMixinVariants.LogoLetters.default}
+			font-size: 22px;
+		`,
+		footerStyle: `
+			${LogoMixinVariants.LogoLetters.default}
+			font-size: 36px;
+		`,
+	},
+};
+
+export const LogoContainer = styled.div<LogoStyledProps>`
 	/* background-color: green; */
-	height: 100%;
-	width: 100%;
-
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`;
-
-export const LogoMiniContainer = styled.span`
-	/* background-color: orange; */
-	color: #ffffff;
+	${(props) => LogoVariants.LogoContainer[props.variant as keyof LogoVariantsType]}
 `;
 
 export const LogoLetters = styled.a<LogoStyledProps>`
-	font-weight: 900;
-
 	${(props) => LogoVariants.LogoLetters[props.variant as keyof LogoVariantsType]}
 `;
 
