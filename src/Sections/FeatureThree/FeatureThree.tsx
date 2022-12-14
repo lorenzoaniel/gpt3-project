496b4c9dbcce7983cae9f2fe864fbade24de8fd6
lorenzoaniel <lorenzoaniel@gmail.com>
2022-12-14 16:17:26 -0700

    Create FeatureThree.tsx
    has the image with 'my apps' and a woman with vr headgear

---
+++ src/Sections/FeatureThree/FeatureThree.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+
+import Image from "../../Components/Image/Image";
+
+import featureImg from "../../Assets/Img/FeatureThree/FeatureImage.png";
+import Paragraph from "../../Components/Paragraph/Paragraph";
+import Heading from "../../Components/Heading/Heading";
+
+const FeatureThree = () => {
+	return (
+		<FT.mainContainer>
+			<FT.miniLeftContainer>
+				<Image srcProp={featureImg} />
+			</FT.miniLeftContainer>
+			<FT.miniRightContainer>
+				<Paragraph
+					contentProp={"Request Early Access to Get Started"}
+					variant={"featureThreeLightBlue"}
+				/>
+				<Heading
+					headingTitleProp={"The possibilities are beyond your imagination"}
+					variant={"featureThreeRainbow"}
+				/>
+				<Paragraph
+					contentProp={
+						"Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of."
+					}
+					variant={"featureThree"}
+				/>
+				<Paragraph
+					contentProp={"Request Early Access to Get Started"}
+					variant={"featureThreeOrange"}
+				/>
+			</FT.miniRightContainer>
+		</FT.mainContainer>
+	);
+};
+
+const FT = {
+	mainContainer: styled.section`
+		/* background-color: black; */
+		height: 750px;
+		width: 100%;
+
+		display: flex;
+
+		padding-top: 100px;
+	`,
+	miniLeftContainer: styled.aside`
+		/* background-color: orange; */
+		height: 100%;
+		width: 55%;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	`,
+	miniRightContainer: styled.aside`
+		/* background-color: red; */
+		height: 100%;
+		width: 45%;
+
+		padding-top: 250px;
+		display: flex;
+		flex-direction: column;
+		row-gap: 5%;
+	`,
+};
+
+export default FeatureThree;
